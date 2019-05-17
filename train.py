e76d16de2778e726b0b016ab12adda4ef69e41cc
viet1pro <29736874+viet1pro@users.noreply.github.com>
2019-05-17 17:55:37 +0700

    Fix shape error
    For Vietnamese plate
    Test 3

--- train.py
+++ train.py
@@ -64,7 +64,8 @@ def read_data(img_glob):
     for fname in sorted(glob.glob(img_glob)):
         im = cv2.imread(fname)[:, :, 0].astype(numpy.float32) / 255.
         code = fname.split("/")[1][9:17]
-        p = fname.split("/")[1][19] == '1'
+        p = fname.split("/")[1][18] == '1'
+        print(p)
         yield im, code_to_vec(p, code)
 
 
